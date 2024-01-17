b2aa337a863b7721a699b4bb1e9a107ba3df88b0
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-17 17:55:35 -0500

    started adding test and test variables

--- api/jokes.test.js
+++ api/jokes.test.js
@@ -2,6 +2,9 @@ const request = require('supertest')
 const db = require('../data/db-config')
 const server = ('./server.js')
 
+const joke1 = {joke: "Porque el maiz se fue al hospital?", punchline:"Esta malito"}
+const joke2 = {joke:'Why was seven afraid of eight?', punchline:'Because seven eight nine!'}
+
 it("correct env variable", () => {
     expect(process.env.NODE_ENV).toBe('testing')
 })
