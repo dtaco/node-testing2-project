3bce78e1a4996a5759925024faec66637a939f8f
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-17 15:25:56 -0500

    more deps added, server built, knex configured

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+const sharedConfig = {
+    client: 'sqlite3',
+    migrations: { directory: './data/migrations' },
+    seeds:{ directory: './data/migrations',}
+    useNullAsDefault: true,
+    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done)}
+}
+
+module.exports = {
+    development: {
+        ...sharedConfig,
+        connection: {
+            filename: './data/jokes.db3'
+        },
+    },
+    testing : {
+        ...sharedConfig,
+        connection: {
+            filename: './data/jokes.test.db3',
+        }
+    }
+};
